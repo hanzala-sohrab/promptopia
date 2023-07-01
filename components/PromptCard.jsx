9175fa8137a2a2ae237d6a21daae3edc4492d424
Hanzala Sohrab <sorb.hanzala@gmail.com>
2023-07-01 17:08:08 +0530

    Add CRUD prompt functionality

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -5,6 +5,10 @@ import { useSession } from "next-auth/react";
 import { usePathname, useRouter } from "next/navigation";
 
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
+  const { data: session } = useSession();
+  const pathName = usePathname();
+  const router = useRouter();
+
   const [copied, setCopied] = useState("");
   const handleCopy = () => {
     setCopied(post.prompt);
@@ -32,6 +36,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
             src={copied === post.prompt ? "/assets/icons/tick.svg" : "/assets/icons/copy.svg"}
             width={12}
             height={12}
+            alt="copy-btn"
           />
         </div>
       </div>
@@ -40,8 +45,19 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
         className="font-inter text-sm cursor-pointer blue_gradient"
         onClick={() => handleTagClick && handleTagClick(post.tag)}
       >
-        {post.tag}
+        #{post.tag}
       </p>
+
+      {session?.user.id === post.creator._id && pathName === "/profile" && (
+        <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
+          <p className="font-inter text-sm green_gradient cursor-pointer" onClick={handleEdit}>
+            Edit
+          </p>
+          <p className="font-inter text-sm orange_gradient cursor-pointer" onClick={handleDelete}>
+            Delete
+          </p>
+        </div>
+      )}
     </div>
   );
 };
